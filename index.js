3b6867140e094af8e28fa4c80924057b6f5dece2
Ashish Garg <mragarg@gmail.com>
2019-04-10 10:10:57 -0400

    Sets Up file for development

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+require('dotenv').config();
 const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
+
+// Routers
+const userRouter = require('./routes/user');
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Helmet
+const helmet = require('helmet');
+app.use(helmet());
+
+// For PORT. Configures express to use built-in middleware that 
+// can deal with form data.
+app.use(express.urlencoded({extended: true}));
+
+// ES6
+app.engine('html', es6Renderer);
+app.set('view engine', 'html');
+app.set('views', 'views');
+
+// GET Home
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// User Router
+
+
+// App Listen
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}.`)
+});

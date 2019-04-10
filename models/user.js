ff73c93d471611a4d55f6177ce52c3efc675b12a
Ashish Garg <mragarg@gmail.com>
2019-04-10 11:07:13 -0400

    Adds user constructor to model/user

--- models/user.js
+++ models/user.js
@@ -3,6 +3,15 @@ const bcrypt = require('bcryptjs');
 
 class User {
 
+    constructor(id, first_name, last_name, email, password) {
+        this.id = id;
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.email = email;
+        this.password = password;
+    }
+
+    
 }
 
 module.exports = User;

58700f175b0de99a99d8261a8d495066a3cf7711
Ashish Garg <mragarg@gmail.com>
2019-04-10 13:33:25 -0400

    Adds ggit status

--- models/favorite.js
+++ models/favorite.js
@@ -2,8 +2,14 @@ const db = require('./conn');
 
 class Favorite {
 
-    static function getUserFavorites(userId){
-        db.any(`select `);
+    static getUserFavorites(userId){
+        db.any(`select * from favorites f
+        inner join users u
+            on f.users_id = u.id
+        inner join dogs d
+            on f.dog_id = d.id
+        where u.id = ${userId}
+        `);
     }
 
 }

5cbd8cf0553060b6bf634519e0c2bdba523f366e
Seil Cho <seilcho7@hotmail.com>
2019-04-10 14:39:40 -0400

    adds dog model functions

--- models/dog.js
+++ models/dog.js
@@ -1,4 +1,27 @@
 const db = require('./conn');
 
+class Dog {
 
-module.exports = dog;
+    constructor(id, name, breed, age, description, image_url, org_id) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.description = description;
+        this.imageUrl = image_url;
+        this.orgId = org_id;
+    }
+
+    // get all dogs
+    static getAll() {
+        db.any(`select * from dogs`);
+    }
+
+    // get all dogs from an organization
+    static getById(orgId) {
+        db.any(`select * from dogs where org_id=$1`, [orgId]);
+    }
+
+}
+
+module.exports = Dog;

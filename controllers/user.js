94951d8d5840d518139253c3e018dc013ee1ea99
Ashish Garg <mragarg@gmail.com>
2019-04-10 15:56:51 -0400

    CHECK PASSWORD

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,36 @@
 const User = require('../models/user');
+const escapeHtml = require('../utils');
 
 function getLoginPage(req, res) {
-    res.render('user-login');
+    res.render('user-login', {
+        locals: {
+            email: '',
+        }
+    });
+}
+
+async function attemptLogin(req, res) {
+
+    const theEmail = escapeHtml(req.body.email);
+    const thePassword = escapeHtml(req.body.password);
+
+    const theUser = await User.getByEmail(theEmail);
+    const passwordIsCorrect = theUser.checkPassword(thePassword);
+
+    if(passwordIsCorrect) {
+        res.redirect('/user/dashboard');
+    }
+    else {
+        res.render('user-login', {
+            locals: {
+                email: req.body.email
+            }
+        })
+    }
+
 }
 
 module.exports = {
-    getLoginPage
+    getLoginPage,
+    attemptLogin
 }
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,16 @@ const Router = express.Router;
 const userRoutes = Router();
 
 const {
-    getLoginPage
+    getLoginPage,
+    attemptLogin
 } = require('../controllers/user');
 
 userRoutes.get('/login', getLoginPage);
+
+userRoutes.post('/login', attemptLogin);
+
+userRoutes.get('/dashboard', (req, res) => {
+    res.send('test');
+})
 
 module.exports = userRoutes;

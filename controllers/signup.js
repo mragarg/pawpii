fe7ba7a7fcc66589a0067dcd6cfd65f970bf0da7
Ashish Garg <mragarg@gmail.com>
2019-04-12 14:31:11 -0400

    Makes a regular user have NULL for org_id

--- controllers/signup.js
+++ controllers/signup.js
@@ -15,9 +15,9 @@ async function addUser(req, res) {
     const lastName = escape(req.body.lastName);
     const email = escape(req.body.email);
     const password = escape(req.body.password);
-    const addUser = await User.add(firstName, lastName, email, password);
+    const addUser = await User.add(firstName, lastName, email, password, null);
     console.log(addUser);
-    const instanceUser = new User(addUser.id, addUser.first_name, addUser.last_name, addUser.email, addUser.password);
+    const instanceUser = new User(addUser.id, addUser.first_name, addUser.last_name, addUser.email, addUser.password, null);
     console.log(instanceUser);
     // await addUser.save();
     console.log('you did the thing');
@@ -40,10 +40,17 @@ async function addOrganization(req, res) {
     const description = escape(req.body.description);
     const url = escape(req.body.url);
 
+    // Create new Organization
     const addOrgan = await Organization.addOrganization(name, address, city, state, zip, phone, orgEmail, orgPassword, description, url);
     console.log(addOrgan);
-    // const instanceOrgan = new Organization(addOrgan.id, addOrgan.name, addOrgan.address, addOrgan.city, addOrgan.state, addOrgan.zip, addOrgan.phone, addOrgan.email, addOrgan.password, addOrgan.description, addOrgan.website);
-    // console.log(instanceOrgan);
+
+    // This will create an Organization Instance
+    const instanceOrgan = new Organization(addOrgan.id, addOrgan.name, addOrgan.address, addOrgan.city, addOrgan.state, addOrgan.zip, addOrgan.phone, addOrgan.email, addOrgan.password, addOrgan.description, addOrgan.website);
+    console.log(instanceOrgan);
+
+    // Create new User
+    const orgUser = await User.add(instanceOrgan.name, instanceOrgan.name, instanceOrgan.email, instanceOrgan.password, instanceOrgan.id);
+    console.log(orgUser);
 
 }
 

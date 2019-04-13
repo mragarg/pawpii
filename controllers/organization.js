53c49e511620948dba7c5210658f14a548a382dd
Seil Cho <seilcho7@hotmail.com>
2019-04-13 11:54:04 -0400

    fix all nav

--- controllers/organization.js
+++ controllers/organization.js
@@ -123,8 +123,47 @@ async function addDogDB(req, res) {
     }
 }
 
-function deleteDogForm(req, res) {
-    res.render('delete-dog');
+async function deleteDogForm(req, res) {
+    if (req.session.user) {
+        const userInstance = await User.getById(req.session.user);
+        if (userInstance.orgId) {
+            res.render('delete-dog', {
+                locals: {
+                    signup: 'd-none',
+                    login: 'd-none',
+                    favorite: 'd-none',
+                    ad: 'Add / Delete',
+                    dogs: 'Current dogs',
+                    logout: 'Log out',
+                    id: userInstance.orgId
+                }
+            });
+        } else if (userInstance.orgId === null) {
+            res.render('delete-dog', {
+                locals: {
+                    signup: 'd-none',
+                    login: 'd-none',
+                    favorite: 'Favorite',
+                    ad: 'd-none',
+                    dogs: 'd-none',
+                    logout: 'Log out',
+                    id: userInstance.orgId
+                }
+            });
+        }
+    } else {
+        res.render('delete-dog', {
+            locals: {
+                signup: 'Sign up',
+                login: 'Log in',
+                favorite: 'd-none',
+                ad: 'd-none',
+                dogs: 'd-none',
+                logout: 'd-none',
+                id: ''
+            }
+        });
+    }
 }
 
 

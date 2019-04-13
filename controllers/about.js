0467339b819cd67f0eb72ce07c7a55a5ade0daec
Seil Cho <seilcho7@hotmail.com>
2019-04-13 11:04:05 -0400

    fix about

--- controllers/about.js
+++ controllers/about.js
@@ -1,45 +1,49 @@
 const User = require('../models/user');
 
-const userInstance = await User.getById(req.session.user);
-if (userInstance.orgId && req.session.user) {
-    res.render('about', {
-        locals: {
-            message: 'About page.',
-            signup: 'd-none',
-            login: 'd-none',
-            favorite: 'd-none',
-            ad: 'Add / Delete',
-            dogs: 'Current dogs',
-            logout: 'Log out',
-            id: userInstance.orgId
+async function checkLogin(req, res) {
+    if (req.session.user) {
+        const userInstance = await User.getById(req.session.user);
+        if (userInstance.orgId) {
+            res.render('about', {
+                locals: {
+                    message: 'About page.',
+                    signup: 'd-none',
+                    login: 'd-none',
+                    favorite: 'd-none',
+                    ad: 'Add / Delete',
+                    dogs: 'Current dogs',
+                    logout: 'Log out',
+                    id: userInstance.orgId
+                }
+            });
+        } else if (userInstance.orgId === null) {
+            res.render('about', {
+                locals: {
+                    message: 'About page.',
+                    signup: 'd-none',
+                    login: 'd-none',
+                    favorite: 'Favorite',
+                    ad: 'd-none',
+                    dogs: 'd-none',
+                    logout: 'Log out',
+                    id: userInstance.orgId
+                }
+            });
         }
-    });
-} else if (userInstance.orgId === null && req.session.user) {
-    res.render('about', {
-        locals: {
-            message: 'About page.',
-            signup: 'd-none',
-            login: 'd-none',
-            favorite: 'Favorite',
-            ad: 'd-none',
-            dogs: 'd-none',
-            logout: 'Log out',
-            id: userInstance.orgId
-        }
-    });
-} else {
-    res.render('about', {
-        locals: {
-            message: 'About page.',
-            signup: 'Sign up',
-            login: 'Log in',
-            favorite: 'd-none',
-            ad: 'd-none',
-            dogs: 'd-none',
-            logout: 'd-none',
-            id: userInstance.orgId
-        }
-    });
+    } else {
+        res.render('about', {
+            locals: {
+                message: 'About page.',
+                signup: 'Sign up',
+                login: 'Log in',
+                favorite: 'd-none',
+                ad: 'd-none',
+                dogs: 'd-none',
+                logout: 'd-none',
+                id: ''
+            }
+        });
+    }
 }
 
 module.exports = {

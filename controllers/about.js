1e80657fba09c72c89abb000806ba4a611b58103
Seil Cho <seilcho7@hotmail.com>
2019-04-13 10:46:56 -0400

    add

--- controllers/about.js
+++ controllers/about.js
@@ -1,28 +1,45 @@
+const User = require('../models/user');
 
-
-function checkLogin(req, res) {
-    if (req.session.user) {
-        res.render('about', {
-            locals: {
-                message: 'About page.',
-                signup: 'd-none',
-                login: 'd-none',
-                favorite: 'Favorite',
-                logout: 'Log out'
-            }
-        });
-    } else {
-        res.render('about', {
-            locals: {
-                message: 'About page.',
-                signup: 'Sign up',
-                login: 'Log in',
-                favorite: 'd-none',
-                logout: 'd-none'
-
-            }
-        });
-    }
+const userInstance = await User.getById(req.session.user);
+if (userInstance.orgId && req.session.user) {
+    res.render('about', {
+        locals: {
+            message: 'About page.',
+            signup: 'd-none',
+            login: 'd-none',
+            favorite: 'd-none',
+            ad: 'Add / Delete',
+            dogs: 'Current dogs',
+            logout: 'Log out',
+            id: userInstance.orgId
+        }
+    });
+} else if (userInstance.orgId === null && req.session.user) {
+    res.render('about', {
+        locals: {
+            message: 'About page.',
+            signup: 'd-none',
+            login: 'd-none',
+            favorite: 'Favorite',
+            ad: 'd-none',
+            dogs: 'd-none',
+            logout: 'Log out',
+            id: userInstance.orgId
+        }
+    });
+} else {
+    res.render('about', {
+        locals: {
+            message: 'About page.',
+            signup: 'Sign up',
+            login: 'Log in',
+            favorite: 'd-none',
+            ad: 'd-none',
+            dogs: 'd-none',
+            logout: 'd-none',
+            id: userInstance.orgId
+        }
+    });
 }
 
 module.exports = {
